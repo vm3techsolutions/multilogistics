0e11dd2e3df1687b8a71c67ea20ffbb051768236
poojaraut2618 <poojaraut2618@gmail.com>
2025-09-11 15:49:20 +0530

    update server.js

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 
 // Routes
-app.use('/api', router);
+app.use('/', router);
 
 // Start Server
 const PORT = process.env.PORT || 5000;
